61f6b04630d17f3b3b6dc3e9980f86f73bd5f272
lenovo 3050ti <1195800137@qq.com>
2024-04-07 19:43:04 +0800

    自定义算法

--- isaacgymenvs/SRL_Evo_train.py
+++ isaacgymenvs/SRL_Evo_train.py
@@ -97,6 +97,7 @@ def launch_rlg_hydra(cfg: DictConfig):
     from isaacgymenvs.learning import amp_players
     from isaacgymenvs.learning import amp_models
     from isaacgymenvs.learning import amp_network_builder
+    from isaacgymenvs.learning.SRLEvo import srl_continuous
     import isaacgymenvs
 
 
@@ -189,6 +190,7 @@ def launch_rlg_hydra(cfg: DictConfig):
         runner = Runner(algo_observer)
         runner.algo_factory.register_builder('amp_continuous', lambda **kwargs : amp_continuous.AMPAgent(**kwargs))
         runner.player_factory.register_builder('amp_continuous', lambda **kwargs : amp_players.AMPPlayerContinuous(**kwargs))
+        runner.algo_factory.register_builder('srl_continuous', lambda **kwargs : srl_continuous.SRLAgent(**kwargs))
         model_builder.register_model('continuous_amp', lambda network, **kwargs : amp_models.ModelAMPContinuous(network))
         model_builder.register_network('amp', lambda **kwargs : amp_network_builder.AMPBuilder())
 
