ecd747508f171af2dec7c5c4391cc91e08f73f11
809 <1195800137@qq.com>
2024-08-20 21:47:48 +0800

    debug

--- isaacgymenvs/learning/SRLEvo/srl_gym.py
+++ isaacgymenvs/learning/SRLEvo/srl_gym.py
@@ -22,9 +22,7 @@ from rl_games.algos_torch import central_value
 from tensorboardX import SummaryWriter
 from rl_games.common import datasets
 from gym.spaces.box import Box
-# from srl_continuous import SRLAgent
-# from ....model_grammar.humanoid_srl_gen import SRL_mode1
-# from ....model_grammar.RobotModelGen import ModelGenerator
+from srl_continuous import SRLAgent
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../model_grammar')))
@@ -32,32 +30,37 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
 from model_grammar import SRL_mode1,ModelGenerator
  
 
-# class SRLGym(SRLAgent):
-#     def __init__(self, base_name, params):
-#         super.__init__(self, base_name, params)
+class SRLGym(SRLAgent):
+    def __init__(self, base_name, params):
+        super.__init__(self, base_name, params)
+
+    def train(self):
+        
+        self.gen_SRL_mjcf('hsrl_test_pretrain','mode1',self.SRL_designer(), pretrain=True)
+        
 
 
-#     def gen_SRL_mjcf(self, name, srl_mode, srl_params, pretrain = False):
-#         srl_generator = { "mode1": SRL_mode1 }[srl_mode]
-#         srl_R = srl_generator( name=name, pretrain=pretrain, **srl_params)
-#         mjcf_generator = ModelGenerator(srl_R,save_path='..../assets/mjcf/humanoid_srl')
-#         back_load = not pretrain
-#         mjcf_generator.gen_basic_humanoid_xml()
-#         mjcf_generator.get_SRL_dfs(back_load=back_load)
-#         mjcf_generator.generate()
+    def gen_SRL_mjcf(self, name, srl_mode, srl_params, pretrain = False):
+        srl_generator = { "mode1": SRL_mode1 }[srl_mode]
+        srl_R = srl_generator( name=name, pretrain=pretrain, **srl_params)
+        mjcf_generator = ModelGenerator(srl_R,save_path='..../assets/mjcf/humanoid_srl')
+        back_load = not pretrain
+        mjcf_generator.gen_basic_humanoid_xml()
+        mjcf_generator.get_SRL_dfs(back_load=back_load)
+        mjcf_generator.generate()
         
     
-#     def SRL_designer(self,):
-#         # 外肢体形态参数生成函数
+    def SRL_designer(self,):
+        # 外肢体形态参数生成函数
 
-#         srl_params = {
-#                     "first_leg_lenth" : 0.40,
-#                     "first_leg_size"  : 0.03,
-#                     "second_leg_lenth": 0.80,
-#                     "second_leg_size" : 0.03,
-#                     "third_leg_size"  : 0.03,
-#                 }
-#         return srl_params
+        srl_params = {
+                    "first_leg_lenth" : 0.40,
+                    "first_leg_size"  : 0.03,
+                    "second_leg_lenth": 0.80,
+                    "second_leg_size" : 0.03,
+                    "third_leg_size"  : 0.03,
+                }
+        return srl_params
 
 
 if __name__ == '__main__':
