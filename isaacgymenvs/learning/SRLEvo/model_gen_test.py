f7b0d349030df85094db3e21cb8b6b8a93db470d
lyoko <1109940549@qq.com>
2024-09-16 20:21:35 +0800

    model

--- isaacgymenvs/learning/SRLEvo/model_gen_test.py
+++ isaacgymenvs/learning/SRLEvo/model_gen_test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     from model_grammar import SRL_mode1,ModelGenerator
     srl_mode = 'mode1'
     # name = 'humanoid_srl_mode1_p1'
-    name = 'hsrl_GA_314_iter65'
+    name = 'hsrl_GA_314_iter37'
     pretrain = False
     # base_srl_params = {
     #                 "first_leg_lenth" : 0.40,
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     #                 "third_leg_size"  : 0.01,
     #             }    
     srl_params = {
-                    "first_leg_lenth" : 0.3054,
-                    "first_leg_size"  : 0.0226,
-                    "second_leg_lenth": 0.7955,
-                    "second_leg_size" : 0.0220,
+                    "first_leg_lenth" : 0.38374,
+                    "first_leg_size"  : 0.0270,
+                    "second_leg_lenth": 0.8942,
+                    "second_leg_size" : 0.039,
                     "third_leg_size"  : 0.0231,
                 }    
     srl_generator = { "mode1": SRL_mode1 }[srl_mode]
